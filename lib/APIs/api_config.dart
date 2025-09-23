aac929102ea94570fa426c51c9efb93d7358efd6
aumtch1234 <taweechok.k@ku.th>
2025-09-23 13:05:25 +0700

    edit calculateDeliveryFee | OrderpageNow

--- lib/APIs/api_config.dart
+++ lib/APIs/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
-  static const String baseUrl = "http://192.168.1.128:4000/client";
-  static const String SocketUrl = "http://192.168.1.128:4000/socket";
-  static const String HosttUrl = "http://192.168.1.128:4000";
-  
+  static const String baseUrl = "http://192.168.1.129:4000/client";
+  static const String SocketUrl = "http://192.168.1.129:4000/socket";
+  static const String HosttUrl = "http://192.168.1.129:4000";
+  static const String AnalyticsDashboardSalesUrl = "http://192.168.1.129:4000/dashboard/sales";
 }
--- lib/pages/order/OrderNowPage.dart
+++ lib/pages/order/OrderNowPage.dart
@@ -553,8 +553,11 @@ class _OrderNowPageState extends State<OrderNowPage> {
   }
 
   double calculateDeliveryFee(double km) {
-    if (km <= 1) return 5;
-    return (km * 5).floorToDouble(); // 5 บาท/กม. ปัดลง
+    if (km <= 2) return 10;
+    if (km <= 10) return 15;
+    if (km <= 15) return 20;
+    // มากกว่า 15 km → base 20 + (ส่วนที่เกิน * 5)
+    return 20 + ((km - 15).ceil() * 5);
   }
 
   // ใช้ map จาก state โดยตรง
