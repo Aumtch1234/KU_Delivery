d2689357d036ce27c80a2f1f5d12a10320237eb7
Natthawut Suwannasri <natthawut.suw@ku.th>
2025-10-08 17:26:59 +0700

    fix(api): update API URLs to correct server address

--- lib/APIs/api_config.dart
+++ lib/APIs/api_config.dart
@@ -1,8 +1,8 @@
 class ApiConfig {
-  static const String baseUrl = "http://10.5.50.146:4000/client";
-  static const String SocketUrl = "http://10.5.50.146:4000/socket";
-  static const String SocketChatUrl = "http://10.5.50.146:4000";
-  static const String HosttUrl = "http://10.5.50.146:4000";
+  static const String baseUrl = "http://10.185.11.73:4000/client";
+  static const String SocketUrl = "http://10.185.11.73:4000/socket";
+  static const String SocketChatUrl = "http://10.185.11.73:4000";
+  static const String HosttUrl = "http://10.185.11.73:4000";
   static const String AnalyticsDashboardSalesUrl =
-      "http://10.5.50.146:4000/dashboard/sales";
+      "http://10.185.11.73:4000/dashboard/sales";
 }
